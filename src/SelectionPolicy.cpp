253772834d7632898ab103653855a3437ff31b6b
MrKouch <guykou@post.bgu.ac.il>
2024-11-27 20:20:23 +0200

    garbage

--- src/SelectionPolicy.cpp
+++ src/SelectionPolicy.cpp
@@ -12,35 +12,9 @@ using std::string;
 using std::vector;
 using namespace std;
 
-NaiveSelection::NaiveSelection() : lastSelectedIndex(0) {}
 
-const FacilityType& NaiveSelection::selectFacility(const vector<FacilityType>& facilitiesOptions) {
-    int currentIndex = lastSelectedIndex; 
-    lastSelectedIndex++;
-    return facilitiesOptions[currentIndex];
-}
-
-const string NaiveSelection:: toString() const {
-    return to_string(lastSelectedIndex);
-}
-
-// // TODO? (Not in assignment)
-// NaiveSelection* NaiveSelection::clone() {}
-// // TODO
-// NaiveSelection::~NaiveSelection() {}
-
-int distanceCalculator (int lifeQualityScore, int economyScore, int environmentScore) {
-    int max = lifeQualityScore;
-    if(economyScore > max) max = economyScore;
-    if(environmentScore > max) max = environmentScore;
-    int min = lifeQualityScore;
-    if(economyScore < min) min = economyScore;
-    if(environmentScore < min) min = environmentScore;
-    return max-min;
-}
-
-BalancedSelection::BalancedSelection(int currLifeQualityScore, int currEconomyScore, int currEnvironmentScore) :
-    LifeQualityScore(currLifeQualityScore), EconomyScore(currEconomyScore), EnvironmentScore(currEnvironmentScore) {}
+BalancedSelection::BalancedSelection(int currLifeQualityScore, int currEconomyScore, int currEnvironmentScore) 
+    LifeQualityScore(currLifeQualityScore), EconomyScore(currEconomyScore), EnvironmentScore(currEnvironmentScore): {}
 
 const FacilityType& BalancedSelection::selectFacility(const vector<FacilityType>& facilitiesOptions) {
     int min = INT_MAX;
@@ -58,44 +32,20 @@ const FacilityType& BalancedSelection::selectFacility(const vector<FacilityType>
     return facilitiesOptions[index];
 }
 
-const string BalancedSelection:: toString() const {
-    return "Balanced selection toString. DONT FORGET TO IMPLEMENT THE DESTRUCTOR AND THE CLONE METHOD!!!";
+const string BalancedSelection:: toString() {
+    cout << "Balanced selection toString. DONT FORGET TO IMPLEMENT THE DESTRUCTOR AND THE CLONE METHOD!!!"
 }
 
-EconomySelection::EconomySelection() : lastSelectedIndex(0) {}
+public int distanceCalculator (int lifeQualityScore, economyScore, environmentScore) {
+    int max(lifeQualityScore);
+    if(economyScore > max) max(economyScore);
+    if(environmentScore > max) max(environmentScore);
+    int min(lifeQualityScore);
+    if(economyScore < min) min(economyScore);
+    if(environmentScore < min) min(environmentScore);
+    return max-min;
+}
 
-const FacilityType& EconomySelection::selectFacility(const vector<FacilityType>& facilitiesOptions) {
-    while (facilitiesOptions[lastSelectedIndex].getCategory() != FacilityCategory::ECONOMY) {
-        lastSelectedIndex++;
-    }
-    int currentIndex = lastSelectedIndex; 
-    lastSelectedIndex++;
-    return facilitiesOptions[currentIndex];
-}
-// TODO? (Not in assignment)
-const string EconomySelection::toString() const {
-    return to_string(lastSelectedIndex);
-}
-// // TODO? (Not in assignment)
-// EconomySelection* EconomySelection::clone() {}
-// // TODO
-// EconomySelection::~EconomySelection() {}
 
-SustainabilitySelection::SustainabilitySelection() : lastSelectedIndex(0) {}
 
-const FacilityType& SustainabilitySelection::selectFacility(const vector<FacilityType>& facilitiesOptions) {
-    while (facilitiesOptions[lastSelectedIndex].getCategory() != FacilityCategory::ENVIRONMENT) {
-        lastSelectedIndex++;
-    }
-    int currentIndex = lastSelectedIndex; 
-    lastSelectedIndex++;
-    return facilitiesOptions[currentIndex];
-}
-// TODO? (Not in assignment)
-const string SustainabilitySelection::toString() const {
-    return to_string(lastSelectedIndex);
-}
-// // TODO? (Not in assignment)
-// SustainabilitySelection* SustainabilitySelection::clone() {}
-// // TODO
-// SustainabilitySelection::~SustainabilitySelection() {}
+
