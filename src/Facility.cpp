5522e7d3a7dd17c63637e42295c70f961bab0259
MrKouch <guykou@post.bgu.ac.il>
2024-11-27 16:12:06 +0200

    Update Facility.cpp

--- src/Facility.cpp
+++ src/Facility.cpp
@@ -3,26 +3,53 @@
 #pragma once
 #include <string>
 #include <vector>
-#include "Facility.h"
+#include <Facility.h>
 
 using std::string;
 using std::vector;
 using namespace std;
 
-class FacilityType {
-    public:
-        FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score):
-        name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), economy_score(economy_score), environment_score(environment_score){}
-  
+// FacilityType
+// Constructors
+// Parameterized
+FacilityType:: FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score):
+    name(name), category(category), price(price), lifeQuality_score(lifeQuality_score), economy_score(economy_score), environment_score(environment_score){}
+
+// Methods
+const string& FacilityType:: getName() {
+    return name;
 }
 
-// Constructor
-Facility::Facility(const string &name, const string &settlementName, const FacilityCategory category,
-const int price, const int lifeQuality_score, const int economy_score, const int environment_score) 
-: name(name), settlementName(settlementName),
-category(category), price(price)
-,lifeQuality_score(lifeQuality_score), economy_score(economy_score),
-environment_score(environment_score) {}
+int Facility:: getCost() const {
+    return price;
+}
+
+int Facility:: getLifeQualityScore() const {
+    return lifeQuality_score;
+}
+
+int Facility:: getEnvironmentScore() const {
+    return environment_score;
+}
+
+int Facility:: getEconomyScore() const {
+    return economy_score;
+}
+
+int Facility:: getCategory() const {
+    return category;
+}
+
+// Facility
+// Constructors
+// Parameterized
+Facility::Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score) : 
+    FacilityType(name, category, price, lifeQuality_score, economy_score, environment_score), settlementName(settlementName), status(UNDER_CONSTRUCTIONS), timeLeft(price) {}
+
+// Partial
+Facility:: Facility(FacilityType &type, const string &settlementName) :
+    Facility(type.name, settlementName, type.category, type.price, type.lifeQuality_score, type.economy_score, type.environment_score) {}
+
 
 const string& Facility::getSettlementName() const {
     return settlementName;
