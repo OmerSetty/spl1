f58331cd39a2272d93752e831c4b3d2be9ff1bc7
setty <setty@post.bgu.ac.il>
2024-11-28 16:03:00 +0000

    add Plan file

---
+++ src/Plan.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#pragma once
+#include <string>
+#include <vector>
+#include <climits>
+
+#include "Facility.h"
+#include "Settlement.h"
+#include "SelectionPolicy.h"
+
+using std::string;
+using std::vector;
+using namespace std;
+
+Plan::Plan() : {}
+
