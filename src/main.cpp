bbc47a821b869a520fad2854bd9aacc459147708
setty <setty@post.bgu.ac.il>
2024-11-30 12:07:49 +0000

    continue plan

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char** argv){
     FacilityType facilityT2 = FacilityType("setty2", FacilityCategory::ENVIRONMENT, 3, 6, 4, 6);//7,5,8=3
     FacilityType facilityT3 = FacilityType("setty3", FacilityCategory::ENVIRONMENT, 8, 2, 3, 0);//3,4,2=2
     FacilityType facilityT4 = FacilityType("setty4", FacilityCategory::ECONOMY, 20, 5, 5, 4);//3,4,6=3
-    FacilityType facilityT5 = FacilityType("setty5", FacilityCategory::LIFE_QUALITY, 20, 4, 7, 3);//5,8,5=3
+    FacilityType facilityT5 = FacilityType("setty5", FacilityCategory::ENVIRONMENT, 20, 4, 7, 3);//5,8,5=3
     FacilityType facilityT6 = FacilityType("setty6", FacilityCategory::ECONOMY, 20, 4, 4, 4);//5,5,6=1
 
     Facility facility = Facility("abc", settlement.getName(),
@@ -41,7 +41,6 @@ int main(int argc, char** argv){
     // cout << bs.selectFacility(facilitiesOptions).getName() << endl;
     // cout << bs.selectFacility(facilitiesOptions).getName() << endl;
 
-    vector<Facility*> underConstruction = { new Facility(facilityT, s), new Facility(facilityT2, s), new Facility(facilityT3, s) };
     Plan* p1 = new Plan(100, settlement, new NaiveSelection(), facilitiesOptions);
     cout << (*p1).toString() << endl;
     cout << (*p1).getEconomyScore() << endl;
@@ -49,6 +48,7 @@ int main(int argc, char** argv){
     (*p1).step();
     cout << "step 2:" << endl;
     (*p1).step();
+    (*p1).setSelectionPolicy(new SustainabilitySelection());
     cout << "step 3:" << endl;
     (*p1).step();
     cout << "step 4:" << endl;
@@ -57,7 +57,6 @@ int main(int argc, char** argv){
     (*p1).step();
     cout << "step 6:" << endl;
     (*p1).step();
-
 
     // cout << ss.selectFacility(facilitiesOptions).getName() << endl;
 
